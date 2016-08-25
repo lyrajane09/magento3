99e3bd5452285087ac93a51378f3d64f86298948
lyrajane09 <bonaoglyra@gmail.com>
2016-08-26 00:48:45 +0800

    add weblog module

---
+++ app/code/local/Weblog/Blogtesting/controllers/IndexController.php
@@ -0,0 +1,11 @@
+<?php
+class Weblog_Blogtesting_IndexController extends Mage_Core_Controller_Front_Action {
+    public function indexAction() {
+        echo 'Setup!';
+    }
+	
+	public function testModelAction() {
+        echo 'Setup!';
+    }
+	
+}
